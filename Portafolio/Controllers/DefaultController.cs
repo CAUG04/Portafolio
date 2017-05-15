f7b5475b42c7e9859f034cd317ce09fd6a2a4da8
Carlos Urrego <carlos.urrego@tekus.co>
2017-05-15 18:52:55 -0500

    Se agregan las librerias y las conexiones a la BD

---
+++ Portafolio/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portafolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //private TablaDato tabladato = new TablaDato();
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            //var paises = tabladato.
+            return View();
+        }
+    }
+}
