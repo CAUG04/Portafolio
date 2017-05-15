e0f03a6eb26cefb7b88f1d0609f259c1d8554fc0
Carlos Urrego <carlos.urrego@tekus.co>
2017-05-15 18:51:25 -0500

    Se agregan los modelos de la BD

--- Model/Properties/AssemblyInfo.cs
+++ Model/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("da28dbfd-4346-4490-9540-5f41553a2650")]
+[assembly: Guid("1f0a1050-6406-4558-b694-f7a2d217a618")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
