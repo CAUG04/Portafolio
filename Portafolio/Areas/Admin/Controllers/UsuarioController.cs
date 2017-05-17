57ccbd286141ff71937103a62683b092ab0dcb94
Carlos Urrego <carlos.urrego@tekus.co>
2017-05-16 20:41:13 -0500

    se agrega seguridad a los controladores

--- Portafolio/Areas/Admin/Controllers/UsuarioController.cs
+++ Portafolio/Areas/Admin/Controllers/UsuarioController.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Portafolio.Areas.Admin.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace Portafolio.Areas.Admin.Controllers
 {
+    [Autenticado]
     public class UsuarioController : Controller
     {
         // GET: Admin/Usuario
