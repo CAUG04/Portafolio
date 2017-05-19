e51543ebdc01e4acd2b2dd3c4c1b6e57c325da15
Carlos Urrego <carlos.urrego@tekus.co>
2017-05-19 09:39:51 -0500

    Controlador de Usuario controller

--- Portafolio/Areas/Admin/Controllers/UsuarioController.cs
+++ Portafolio/Areas/Admin/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Model;
+﻿using Helper;
+using Model;
 using Portafolio.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,14 @@ namespace Portafolio.Areas.Admin.Controllers
     [Autenticado]
     public class UsuarioController : Controller
     {
-        
         private Usuario usuario = new Usuario();
         private TablaDato dato = new TablaDato();
 
         // GET: Admin/Usuario
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Paises = dato.Listar("pais");
+            return View(usuario.Obtener(SessionHelper.GetUser()));
         }
     }
 }
